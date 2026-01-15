fccd7509a7ae677e3d8607f6cadab9b9285c0346
Hugo Reis Santos <hugorsantos@gmail.com>
2026-01-15 08:39:53 -0300

    Criando projeto angular

---
+++ angular-ai/src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatButton} from '@angular/material/button';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, RouterLink, MatToolbar, MatButton],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+
+  protected readonly title = "AI-Spring-Angular"
+
+
+}
